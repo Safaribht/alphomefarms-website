f83af7932efd1238e61bdaab21103314b1c4dd1f
Safaribht <safaribht@gmail.com>
2025-12-18 18:28:15 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Nav from "@/components/Nav";
-import Footer from "@/components/Footer";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 export default function RootLayout({
   children,
